8c24a48db8070531236d5813879df5db616ec12d
Eva Camino <caminoeva1@gmail.com>
2023-03-02 15:49:34 -0700

    adding more js

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -11,68 +11,76 @@
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
-        fiveDay(data.coord.lat, data.coord.lon)
+        fiveDay(data.coord.lat, data.coord.lon);
         cityInput.html("");
         fiveDays.html("");
         displayDay(data);
       });
   }
-  function fiveDay (lat,lon){
+  function fiveDay(lat, lon) {
     const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
     fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data)
-    for (let i = 0; i < data.list.length; i += 8){
-      console.log(data.list[i]);
-    }
-
-
-
-
-    })
-    function displayFive (data) {
-      
-    }
-  } 
-
-  function displayDay(data) {
-    let card = $("<div>");
-    card.addClass("card");
-    let cardBody = $("<div>");
-    card.addClass("card-body");
-    let cardTitle = $("<div>");
-    card.addClass("card-title");
-    let cardText = $("<div>");
-    card.addClass("card-text");
-    cardTitle.text(new Date(data.dt));
-    cardText.html(
-      `<p>tempture: ${data.main.temp}</p><p><p>humidity: ${data.main.humidity}</p><p>wind: ${data.wind.speed}</p>`
-    );
-    cardBody.append(cardTitle).append(cardText);
-
-    card.append(cardBody);
-    $("#oneDayDisplay").append(card);
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        for (let i = 0; i < data.list.length; i += 8) {
+          displayFive(data.list[i]);
+        }
+      });
+  
   }
-  //getWeather();
-  function Searching (){
-    this.getWeather$('#cityInput').value;
+  
+  function displayDay(data) {
+    let card = $("#oneDayDisplay");
+  
+      const cardHtml = `
+      <div class="card-body">
+        <h5 class="card-title">${new Date(data.dt)}</h5>
+        <p>Tempture: ${data.main.temp}</p>
+        <p>Humidity: ${data.main.humidity}</p>
+        <p>Wind: ${data.wind.speed}</p
+      </div>
+      `
+const cardEl = $(cardHtml);
+card.append(cardEl);
+
+  }
+
+  function Searching() {
+    this.getWeather$("#cityInput").value;
   }
 
   document.querySelector("#submit-city").addEventListener("click", function () {
-    const citySearchEl = document.querySelector('#cityInput')
-    //console.log(citySearchEl)
-   // console.dir(citySearchEl)
+    const citySearchEl = document.querySelector("#cityInput");
     const cityText = citySearchEl.value;
     getWeather(cityText);
+    fiveDay(cityText);
   });
-
 })(jQuery);
 // for (let i = 0; i < 5; i++) {
 
 //}
+function displayFive(data) {
+  const cards = $("#five-days");
 
+  const cardHtml = `
+  <div class="col">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">${data.dt}</h5>
+        <p>tempture: ${data.main.temp}</p>
+        <p>humidity: ${data.main.humidity}</p>
+        <p>wind: ${data.wind.speed}</p
+      </div>
+    </div>
+  </div>
+  `
+  const cardEl = $(cardHtml);
+  cards.append(cardEl);
 
+  // cards.append(cardBodys);
+  // $("#five-days").append(cards);
+}
 //</div>
 // {/* <div class="card bg-light mb-3" style="max-width: 18rem;">
 //   <div class="card-header">Header</div>
@@ -81,56 +89,3 @@
 //     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
 //   </div>
 // </div> */}
-
-// // { coord: {… }, weather: Array(1), base: 'stations', main: {… }, visibility: 10000, … }
-// base
-// :
-// "stations"
-// clouds
-// :
-// { all: 96 }
-// cod
-// :
-// 200
-// coord
-// :
-// { lon: -0.1257, lat: 51.5085 }
-// dt
-// :
-// 1677111645
-// id
-// :
-// 2643743
-// main
-// :
-// { temp: 279.16, feels_like: 276.81, temp_min: 277.84, temp_max: 280.01, pressure: 1013, … }
-// name
-// :
-// "London"
-// sys
-// :
-// { type: 2, id: 2075535, country: 'GB', sunrise: 1677135569, sunset: 1677173322 }
-// timezone
-// :
-// 0
-// visibility
-// :
-// 10000
-// weather
-// :
-// Array(1)
-// 0
-// :
-// { id: 804, main: 'Clouds', description: 'overcast clouds', icon: '04n' }
-// length
-// :
-// 1
-// [[Prototype]]
-// :
-// Array(0)
-// wind
-// :
-// { speed: 3.09, deg: 350 }
-// [[Prototype]]
-// :
-// Object
